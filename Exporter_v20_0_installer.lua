8917fd8bae5f2cfed7afdd3c99f09b65d43e179e
luastudio <45271344+luastudio@users.noreply.github.com>
2023-01-20 20:08:36 -0500

    FIx 2

--- Exporter_v20_0_installer.lua
+++ Exporter_v20_0_installer.lua
@@ -9,7 +9,7 @@ request = string.format([[GET %s%s HTTP/1.1
 Host: %s
 Connection: close
 Content-Type: application/x-www-form-urlencoded
-	
+
 ]], path, fileName, host)
 
 processThread = Lib.Sys.VM.Thread.create([[
@@ -17,10 +17,13 @@ init = Lib.Sys.VM.Thread.readMessage(true)
 request = init.request
 host = init.host
 fileName = init.fileName
+
 function round(n)
     return n % 1 >= 0.5 and math.ceil(n) or math.floor(n)
 end
+
 onemore = true
+
 while onemore do
 socket = Lib.Sys.SSL.Socket.new()
 Lib.Sys.trace("Connecting")
@@ -28,6 +31,7 @@ socket.connect( Lib.Sys.Net.Host.new( host ), 443 )
 Lib.Sys.trace("Connected")
 Lib.Sys.trace("Writing header")
 socket.write(request)
+
 -- SKIP the HTTP header (until \r\n\r\n)
 local b = Lib.Sys.IO.BytesBuffer.new()
 local k = 4
@@ -39,17 +43,23 @@ while( true )do
     p = p + socket.input.readBytes(s,p,k - p)
   end 
   b.addBytes(s,0,k)
+
   if( k == 1 )then
+
     local c = s.get(0)
     if( c == 10 )then break end
 	if( c == 13 )then k = 3 else k = 4 end
+
   elseif( k == 2 )then
+
     local c = s.get(1)
     if( c == 10 )then
       if( s.get(0) == 13 )then break end
       k = 4
     elseif( c == 13 )then k = 3 else k = 4 end
+
   elseif( k == 3 )then
+
     local c = s.get(2)
     if( c == 10 )then
       if( s.get(1) ~= 13 )then k = 4
@@ -57,7 +67,9 @@ while( true )do
     elseif( c == 13 )then
       if( s.get(1) ~= 10 or s.get(0) ~= 13 )then k = 1 else k = 3 end
     else k = 4 end
+
   elseif( k == 4 )then
+
     local c = s.get(3)
     if( c == 10 )then
       if( s.get(2) ~= 13 )then goto continue
@@ -65,9 +77,12 @@ while( true )do
     elseif( c == 13 )then
       if( s.get(2) ~= 10 or s.get(1) ~= 13 )then k = 3 else k = 1 end
     end
+
   end
+
   ::continue::
 end
+
 headers = Lib.Str.split(b.getBytes().toString(), "\r\n")
 response = headers[1]
 rp = Lib.Str.split(response, " ")
@@ -76,6 +91,7 @@ if status == 0 or status == nil then
     socket.close()
 	error("Response status error")
 end
+
 onemore = false
 print("http status: "..status)
 if status == 301 or status == 302 then
@@ -89,15 +105,18 @@ if status == 301 or status == 302 then
 		urlo = Lib.Str.parseUrl(url)
 		host = urlo.host
         path = urlo.relative
+
         onemore = true
 		request = string.format("GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n", 
 		path, host)
+
 		break
     end
     i = i + 1
   end
 end --end if
 end --end while onemore
+
 local fOut = Lib.Sys.IO.File.write(Lib.Media.FileSystem.File.documentsDirectory.nativePath.."/"..fileName, true)
 bufSize = 1024 * 10
 buf = Lib.Sys.IO.Bytes.alloc( bufSize )
@@ -114,6 +133,7 @@ while true do
     else
         len = result
     end
+
     if eof or len <= 0 then 
       if eof and pos == 0 then Lib.Sys.trace("empty file or incorrect url") end 
       break
@@ -121,6 +141,7 @@ while true do
       fOut.writeBytes(buf, 0, len) 
       pos = pos + len 
       --Lib.Sys.trace(pos) 
+
       local doneMb = round(pos / (1024 * 1024))
       if(doneMb > mbComplete) then
          Lib.Sys.trace("Loaded " .. doneMb .. " Mb")
